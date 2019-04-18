50449ae796d8c5db3aa52ef9b8cd05fea5ee0a44
Naveen S <naveen2aug@gmail.com>
2019-04-18 22:21:03 +0530

    Update main.js

--- logo/main.js
+++ logo/main.js
@@ -18,7 +18,7 @@ var initParticleSlider = function(){
     : psScript.onload = init);
   psScript.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/23500/ps-0.9.js';
 	psScript.setAttribute('type', 'text/javascript');
-  document.getElementsByTagName(body).appendChild(psScript);
+  document.getElementsByTagName("body")[0].appendChild(psScript);
 }
     
 (window.addEventListener
