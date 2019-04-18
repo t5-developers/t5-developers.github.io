91a3ab123bcc9aa3dad6edbaa23d2d392bf2a483
Naveen S <naveen2aug@gmail.com>
2019-04-18 21:41:45 +0530

    Update main.js

--- logo/main.js
+++ logo/main.js
@@ -29,17 +29,3 @@ var init = function(){
    ? window.addEventListener('click', function(){ps.init(true)}, false)
    : window.onclick = function(){ps.init(true)});
 }
-
-var initParticleSlider = function(){
-  var psScript = document.createElement('script');
-  (psScript.addEventListener
-    ? psScript.addEventListener('load', init, false)
-    : psScript.onload = init);
-  psScript.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/23500/ps-0.9.js';
-	psScript.setAttribute('type', 'text/javascript');
-  document.body.appendChild(psScript);
-}
-    
-(window.addEventListener
-  ? window.addEventListener('load', initParticleSlider, false)
-  : window.onload = initParticleSlider);
