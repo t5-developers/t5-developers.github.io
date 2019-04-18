e5ce428e1ab17a8477b66d6412b88e62c279d5f6
Naveen S <naveen2aug@gmail.com>
2019-04-18 22:42:12 +0530

    Update main.js

--- logo/main.js
+++ logo/main.js
@@ -9,6 +9,24 @@ var init = function(){
     width: 1e9,
     height: 1e9
   });
+  var gui = new dat.GUI();
+  gui.add(ps, 'ptlGap').min(0).max(5).step(1).onChange(function(){
+    ps.init(true);
+  });
+  gui.add(ps, 'ptlSize').min(1).max(5).step(1).onChange(function(){
+    ps.init(true);
+  });
+  gui.add(ps, 'restless');
+  gui.addColor(ps, 'color').onChange(function(value){
+    ps.monochrome = true;
+    ps.setColor(value);
+	  ps.init(true);
+  });
+  
+  
+  (window.addEventListener
+   ? window.addEventListener('click', function(){ps.init(true)}, false)
+   : window.onclick = function(){ps.init(true)});
 }
 
 var initParticleSlider = function(){
